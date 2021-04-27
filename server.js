67f0dd9d0d9495d7de37925fa06e2f9ff489bd08
Jesse Orozco <orozcojesse0@gmail.com>
2021-04-26 20:37:16 -0400

    added code to css file and server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./routes/routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
