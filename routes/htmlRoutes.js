8c73e33023517cf786a9442c4d19e30cfe5d7923
Jesse Orozco <orozcojesse0@gmail.com>
2021-04-28 21:08:27 -0400

    minor touches to index and not file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 module.export = (app) => {
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/..public/index.html"));
   });
 
-  app.get("/", (req, res) => {
+  app.get("/notes", (req, res) => {
     res.sendFile(path.joib(__dirname, "/..public/notes.html"));
   });
 };
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -10,7 +9,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/apiRoutes")(app);
-require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
